286b8f65a55895ab70854f8f4a5938044af78353
owkahin <mahamedwarsamed69@gmail.com>
2024-02-12 17:01:11 +0300

    done

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let body = document.querySelector('body');
+let hujale = document.createElement('h1');
+let paragaph = document.createElement('p')
+
+hujale.innerText = 'hellow';
+
+
+
+
+body.appendChild(hujale)
